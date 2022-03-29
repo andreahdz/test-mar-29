3e76a5a018df8adcffcd8f38cae2852e3d15f355
Andrea <andrea.hernandez@shopify.com>
2022-03-29 13:20:06 -0700

    Update shopify.config.js

--- shopify.config.js
+++ shopify.config.js
@@ -1,6 +1,5 @@
 export default {
   defaultLocale: 'en',
   storeDomain: 'trial-store-for-andrea.myshopify.com',
-  storefrontToken: '386c137fbe0cef8cf51613c41e4726d9',
   storefrontApiVersion: 'unstable',
 };
